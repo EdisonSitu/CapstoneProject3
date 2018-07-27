f00cd5ac6518f75e709d860a591b06c6306b4f44
Edison Situ <situe@bxscience.edu>
2018-07-27 09:29:43 -0400

    Added display

---
+++ display.py
@@ -0,0 +1,7 @@
+def display(image_ids):
+    urls
+    for i in image data
+        append.dict[askfhasknlsDFN]get coco_url
+    fig, ax = plt.subplot(rows = len(urls))
+    for i, link in enumerate(urls)
+        ax.plot[i](link)
